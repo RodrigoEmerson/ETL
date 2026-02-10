56bea60bfe2b8587cd5da5fb823f09f7bd1ae98e
RodrigoEmerson <rodrigo_belon@hotmail.com>
2026-02-10 17:25:02 -0300

    commit atualizar

--- extracao.py
+++ extracao.py
@@ -2,19 +2,30 @@ import pandas as pd
 import requests
 import json
 
-api_url = "Github"
+api_url = "https://raw.githubusercontent.com/RodrigoEmerson/ETL/main/url_id_JSON.json"
 
 df = pd.read_csv('dadosID.csv')
 user_ids = df['UserID'].tolist()
-#print(users_ids)
-responde = requests.get(f'{api_url}/{id}')
+print("IDs do CSV:", user_ids)
 
-print(responde)
+response = requests.get(api_url)
+if response.status_code == 200:
+    all_users = response.json()
+    if isinstance(all_users, str):
+        all_users = json.loads(all_users)  # 👈 decodifica a string JSON
+else:
+    raise Exception(f"Erro ao acessar JSON: {response.status_code}")
 
-def get_user(id):
-    responde = requests.get(f'{api_url}/id/{id}')
-    return responde.json() if responde.status_code == 200 else None
+print("Tipo de all_users:", type(all_users))
+print("Conteúdo de all_users:", all_users)
 
-#id = [user for id in user_ids if (user := get_user(id)) is not None]
+def get_user(user_id):
+    for user in all_users:
+        if user["id"] == user_id:
+            return user
+    return None
 
-print(get_user(1))
+users = [user for id in user_ids if (user := get_user(id)) is not None]
+
+print("Usuários encontrados:")
+print(json.dumps(users, indent=2, ensure_ascii=False))
